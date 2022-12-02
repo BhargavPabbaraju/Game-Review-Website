3f110cfb2971efc1f25a89598e1266a5f766b460
Kiran  Shatiya <kiranshatiya@Kirans-MacBook-Pro.local>
2022-12-02 18:28:03 -0500

    Minor changes for view your games page

--- src/view-created-games/index.js
+++ src/view-created-games/index.js
@@ -6,22 +6,22 @@ const ViewGameComponent = ()=>{
   const results=[{
     _id:123,
     active:false,
-    image:"https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg",
-    title:"Grand Theft Auto V",
+    background_image:"https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg",
+    name:"Grand Theft Auto V",
     rating:4.47,
   },
     {
       _id:234,
       active:true,
-      image:"https://media.rawg.io/media/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg",
-      title:"The Witcher 3: Wild Hunt",
+      background_image:"https://media.rawg.io/media/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg",
+      name:"The Witcher 3: Wild Hunt",
       rating:4.06,
     },
     {
       _id:345,
       active:false,
-      image:"https://media.rawg.io/media/games/328/3283617cb7d75d67257fc58339188742.jpg",
-      title:"Portal 2",
+      background_image:"https://media.rawg.io/media/games/328/3283617cb7d75d67257fc58339188742.jpg",
+      name:"Portal 2",
       rating:4.61,
     }, ]
   return(
